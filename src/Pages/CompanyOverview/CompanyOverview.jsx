33972ccc133cf32fe2f3be51d7f36edeafe684d0
Rishav Pandey <contactrishavpandey@gmail.com>
2024-01-18 20:35:02 +0530

    Added overview subsections

--- src/Pages/CompanyOverview/CompanyOverview.jsx
+++ src/Pages/CompanyOverview/CompanyOverview.jsx
@@ -2,15 +2,53 @@ import { useParams } from "react-router-dom";
 
 import { COMPANY_OVERVIEW_DATA } from "../../assets/CompanyOverviewData";
 
+import "./CompanyOverview.css";
+
 const CompanyOverview = () => {
   const { company } = useParams();
+  const CompanyLogo = COMPANY_OVERVIEW_DATA[company].logo;
   return (
-    <div>
-      {COMPANY_OVERVIEW_DATA[company].logo}
-      <h1>{COMPANY_OVERVIEW_DATA[company].name}</h1>
-      {COMPANY_OVERVIEW_DATA[company].description.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
-      ))}
+    <div className="company-overview-wrapper">
+      <div
+        style={{
+          width: "max-content",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="company-logo-frame">
+          <CompanyLogo className="company-logo" />
+        </div>
+        <span className="company-name">
+          {COMPANY_OVERVIEW_DATA[company].name}
+        </span>
+      </div>
+      <div className="sub-section">
+        <div className="sub-section-title-wrapper">
+          <hr />
+          <span className="sub-section-title">About</span>
+          <hr />
+        </div>
+        {COMPANY_OVERVIEW_DATA[company].description.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
+      <div className="sub-section">
+        <div className="sub-section-title-wrapper">
+          <hr />
+          <span className="sub-section-title">Campus History</span>
+          <hr />
+        </div>
+      </div>
+      <div className="sub-section">
+        <div className="sub-section-title-wrapper">
+          <hr />
+          <span className="sub-section-title">Roles Recruited</span>
+          <hr />
+        </div>
+      </div>
     </div>
   );
 };
--- src/assets/CompanyOverviewData.js
+++ src/assets/CompanyOverviewData.js
@@ -8,7 +8,7 @@ export const COMPANY_OVERVIEW_DATA = {
   tcs: {
     name: "Tata Consultancy Services",
     abbreviation: "TCS",
-    logo: <TCSLogo />,
+    logo: TCSLogo,
     description: [
       "Tata Consultancy Services (TCS) is a global powerhouse in the IT services and consulting industry, boasting a legacy that spans back to its establishment in 1968. TCS has consistently been at the forefront of digital transformation, delivering cutting-edge solutions to a diverse clientele. As an employee, you'll find yourself immersed in a culture that thrives on innovation, collaboration, and continuous learning. TCS places a strong emphasis on professional development, offering comprehensive training programs and mentorship opportunities to help you grow in your career. Joining TCS means becoming part of a global community of tech enthusiasts, where your skills are valued, and you contribute to meaningful projects that shape the future of technology.",
       "Diversity and inclusion are integral to TCS's identity, creating a workplace where individuals from diverse backgrounds come together to drive success. TCS understands the importance of work-life balance and offers flexible work arrangements to accommodate various needs. The company's commitment to social responsibility is evident through initiatives contributing to community development and environmental sustainability. As a TCS employee, you're not just part of a corporation; you're an integral member of a socially responsible and forward-thinking community that values your unique skills and perspectives. Embark on a transformative journey with TCS, where your career aspirations align with a global leader that continues to redefine the possibilities of the digital era.",
@@ -18,7 +18,7 @@ export const COMPANY_OVERVIEW_DATA = {
   ubs: {
     name: "Union Bank of Switzerland",
     abbreviation: "UBS",
-    logo: <UBSLogo />,
+    logo: UBSLogo,
     description: [
       "Union Bank of Switzerland (UBS) stands as a venerable institution in the global financial sector, tracing its roots back to 1856. Today, UBS is a leading financial powerhouse, providing professionals with an unparalleled opportunity to thrive at the intersection of finance and innovation. As a UBS employee, you'll have the chance to immerse yourself in a world of finance where precision, expertise, and client-centricity are paramount. The company's commitment to employee development is reflected in its comprehensive training programs, mentorship opportunities, and a culture that values continuous learning and professional growth.",
       "Working at UBS means collaborating with a diverse and talented team, contributing to impactful projects that shape the global financial landscape. UBS places a strong emphasis on fostering a work environment that encourages innovation, diversity, and inclusion. As a responsible corporate citizen, UBS is dedicated to sustainable practices, corporate social responsibility, and community engagement. Joining UBS means not only advancing your career in finance but also becoming part of a global network of professionals who are passionate about making a positive impact. Experience a rewarding journey with UBS, where your expertise and aspirations align with a financial powerhouse that continues to drive excellence in the ever-evolving world of finance.",
@@ -28,7 +28,7 @@ export const COMPANY_OVERVIEW_DATA = {
   ust: {
     name: "UST",
     abbreviation: "UST",
-    logo: <USTLogo />,
+    logo: USTLogo,
     description: [
       "UST, formerly known as UST Global, is a dynamic and innovative digital transformation company that has carved its niche since its establishment in 1999. UST has rapidly evolved into a key player in the global IT services industry, providing cutting-edge solutions to clients across diverse sectors. As a UST employee, you'll find yourself at the forefront of technological advancements, working on transformative projects that push the boundaries of innovation. The company's commitment to fostering a culture of creativity, collaboration, and continuous learning is evident in its various programs aimed at skill enhancement, career progression, and overall personal development.",
       "UST values its employees as its greatest asset, creating an inclusive and supportive work environment where individual contributions are recognized and celebrated. The company is dedicated to staying ahead in the rapidly evolving tech landscape and encourages its employees to embrace change, adapt to emerging technologies, and drive innovation. UST's emphasis on work-life balance, coupled with a plethora of career development opportunities, makes it an ideal workplace for tech enthusiasts looking to advance their careers. Join UST to be part of a forward-thinking organization where your expertise is recognized, and your contributions play a crucial role in shaping the future of technology.",
